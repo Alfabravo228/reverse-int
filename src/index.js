4f369210b6c1c47508f92fd704857a2a5742e63e
Bohdan <6alfabravo9@gmail.com>
2021-03-11 14:47:07 +0200

    create a function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  if (n == null || n == "") {
+    return 0;
+  }
+  if (n < 0) {
+    n *= -1;
+  }
+  let str = String(n);
+  console.log(typeof str);
+  let arr1 = str.split("");
+  let arr2 = [];
+  for (let i = 0; i < arr1.length; i++) {
+    arr2.unshift(arr1[i]);
+  }
+  str = arr2.join('');
+  return Number(str);
 }
+
+// console.log(reverse(234543));
